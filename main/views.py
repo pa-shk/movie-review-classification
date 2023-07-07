8dddcdfb68134344feb34566ebedc3014d68614c
Pavel Shkunov <pvlshkunov@gmail.com>
2023-07-08 02:31:33 +0300

    ml

--- main/views.py
+++ main/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+import pickle
 
 # Create your views here.
 def home(request):
@@ -6,7 +7,18 @@ def home(request):
 
 def user(request):
     review = request.GET['review']
-    print(review)
-    rating = 10
-    sentiment = 'positive'
+    rating = predict_rating(review)
+    sentiment = predict_sentiment(review)
     return render(request, 'response.html', {'sentiment': sentiment, 'score': rating})
+
+
+def predict_rating(text):
+    with open('rating_predictor', 'rb') as f:
+        model = pickle.load(f)
+    return model.predict([text])[0]
+
+
+def predict_sentiment(text):
+    with open('binary_classifier', 'rb') as f:
+        model = pickle.load(f)
+    return  'negative' if model.predict([text])[0] else 'positive'
