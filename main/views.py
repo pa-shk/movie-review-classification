88c7440ddd9f6ebb53a4c9971e30869f5c98c960
Pavel Shkunov <pvlshkunov@gmail.com>
2023-07-08 21:36:58 +0300

    views restructure

--- main/views.py
+++ main/views.py
@@ -1,24 +1,18 @@
 from django.shortcuts import render
 import pickle
 
+with open('rating_predictor', 'rb') as f:
+    RATING_PREDICTOR = pickle.load(f)
+
+with open('binary_classifier', 'rb') as f:
+    BINARY_CLASSIFIER = pickle.load(f)
+
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
 
 def user(request):
     review = request.GET['review']
-    rating = predict_rating(review)
-    sentiment = predict_sentiment(review)
+    rating = RATING_PREDICTOR.predict([review])[0]
+    sentiment = BINARY_CLASSIFIER.predict([review])[0]
     return render(request, 'response.html', {'sentiment': sentiment, 'score': rating})
-
-
-def predict_rating(text):
-    with open('rating_predictor', 'rb') as f:
-        model = pickle.load(f)
-    return model.predict([text])[0]
-
-
-def predict_sentiment(text):
-    with open('binary_classifier', 'rb') as f:
-        model = pickle.load(f)
-    return  'negative' if model.predict([text])[0] else 'positive'
